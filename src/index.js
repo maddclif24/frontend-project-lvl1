0f0cf4e1e555f4ba84bf3cdca7ccdfda99b90338
maddclif <maddclif24@gmail.com>
2020-03-13 23:27:17 +0000

    add_brain-even

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import readlineSync from 'readline-sync';
 
 const whatsYourName = () => {
+  let playerName = '';
   const actual = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${actual}!`);
+  playerName = actual;
+  return playerName;
 };
 
-export default whatsYourName;
+const randomNumber = () => Math.floor(Math.random(1) * 100);
+
+const isEvenNumber = (genRandomNum) => {
+  if (genRandomNum % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+const questionForNumber = (genRandomNum) => {
+  console.log(`Question: ${genRandomNum}`);
+  const answer = readlineSync.question('Your answer: ');
+  return answer;
+};
+
+const evenGame = (genRandomNum, name) => {
+  for (let correctAnswer = 0; correctAnswer < 3; genRandomNum = randomNumber()) {
+    const playerAnswer = questionForNumber(genRandomNum);
+    if (playerAnswer === isEvenNumber(genRandomNum)) {
+      correctAnswer += 1;
+      console.log('Correct!');
+    } else if (playerAnswer !== isEvenNumber(genRandomNum)) {
+      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${isEvenNumber(genRandomNum)}`);
+      correctAnswer = 0;
+    }
+  }
+  console.log(`Congratulation, ${name}!`);
+};
+
+const game = () => {
+  const genRandomNum = randomNumber();
+  console.log('Welcome to the Brain Games!');
+  const name = whatsYourName();
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
+  evenGame(genRandomNum, name);
+};
+export default game;
